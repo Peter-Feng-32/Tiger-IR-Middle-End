eb7e3e65aacc96bf79c4c7982645d4a0f2ea2f46
Peter Feng <37753741+Coypirus@users.noreply.github.com>
2022-01-24 22:53:37 -0500

    Create Project

---
+++ README.md
@@ -0,0 +1,7 @@
+# TigerIRMiddleEnd
+
+Middle end of a compiler that performs static analysis and code optimization of Tiger-IR
+
+## Contributing
+
+Take a look at our [Contributing Guide](CONTRIBUTING.md).
---
+++ lib/tigerirmiddleend.mli
@@ -0,0 +1,8 @@
+(** Middle end of a compiler that performs static analysis and code optimization of Tiger-IR *)
+
+val greet : string -> string
+(** Returns a greeting message.
+
+    {4 Examples}
+
+    {[ print_endline @@ greet "Jane" ]} *)
