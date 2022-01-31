48bf98882746536da09d9b5a0ddb5cff4e63ab2d
Srikar Vanavasam <svanavasam3@gatech.edu>
2022-01-31 07:11:39 -0500

    Added make_cfg

---
+++ bin/cfg.ml
@@ -0,0 +1,161 @@
+open Core
+open Syntax
+
+module Node = struct
+  type t = instruction * int
+
+  let compare = Stdlib.compare
+  let hash = Hashtbl.hash
+  let equal = phys_equal
+  let index = -1
+end
+
+module Edge = struct
+  type t = string
+
+  let compare = Stdlib.compare
+  let equal = ( = )
+  let default = ""
+end
+
+let string_of_operand o =
+  match o with
+  | Int a -> string_of_int a
+  | Float a -> string_of_float a
+  | Identifier a -> a
+
+let string_of_instruction i =
+  match i with
+  | Label a -> a
+  | Goto a -> a
+  | Assign (a, b) -> String.concat ~sep:"," [ "assign"; a; string_of_operand b ]
+  | Add (a, b, c) ->
+      String.concat ~sep:","
+        [ "add"; a; string_of_operand b; string_of_operand c ]
+  | Sub (a, b, c) ->
+      String.concat ~sep:","
+        [ "sub"; a; string_of_operand b; string_of_operand c ]
+  | Mult (a, b, c) ->
+      String.concat ~sep:","
+        [ "mult"; a; string_of_operand b; string_of_operand c ]
+  | Div (a, b, c) ->
+      String.concat ~sep:","
+        [ "div"; a; string_of_operand b; string_of_operand c ]
+  | And (a, b, c) ->
+      String.concat ~sep:","
+        [ "and"; a; string_of_operand b; string_of_operand c ]
+  | Or (a, b, c) ->
+      String.concat ~sep:","
+        [ "or"; a; string_of_operand b; string_of_operand c ]
+  | Breq (a, b, c) ->
+      String.concat ~sep:","
+        [ "breq"; a; string_of_operand b; string_of_operand c ]
+  | Brneq (a, b, c) ->
+      String.concat ~sep:","
+        [ "brneq"; a; string_of_operand b; string_of_operand c ]
+  | Brlt (a, b, c) ->
+      String.concat ~sep:","
+        [ "brlt"; a; string_of_operand b; string_of_operand c ]
+  | Brgt (a, b, c) ->
+      String.concat ~sep:","
+        [ "brgt"; a; string_of_operand b; string_of_operand c ]
+  | Brgeq (a, b, c) ->
+      String.concat ~sep:","
+        [ "breq"; a; string_of_operand b; string_of_operand c ]
+  | Brleq (a, b, c) ->
+      String.concat ~sep:","
+        [ "brleq"; a; string_of_operand b; string_of_operand c ]
+  | Return a -> "return" ^ string_of_operand a
+  | Call (a, b) ->
+      String.concat ~sep:"," ("call" :: a :: List.map ~f:string_of_operand b)
+  | Callr (a, b, c) ->
+      String.concat ~sep:","
+        ("callr" :: a :: b :: List.map ~f:string_of_operand c)
+  | Array_Store (a, b, c) ->
+      String.concat ~sep:","
+        [ "array_store"; string_of_operand a; b; string_of_operand c ]
+  | Array_Load (a, b, c) ->
+      String.concat ~sep:","
+        [ "array_load"; string_of_operand a; b; string_of_operand c ]
+  | Array_Assign (a, b, c) ->
+      String.concat ~sep:","
+        [ "array_assign"; a; string_of_int b; string_of_operand c ]
+
+module G = Graph.Persistent.Digraph.ConcreteBidirectionalLabeled (Node) (Edge)
+
+let string_of_vertex v =
+  let a, b = v in
+  string_of_int b ^ ": " ^ string_of_instruction a
+
+module Dot = Graph.Graphviz.Dot (struct
+  include G (* use the graph module from above *)
+
+  let edge_attributes (_, e, _) = [ `Label e; `Color 4711 ]
+  let default_edge_attributes _ = []
+  let get_subgraph _ = None
+  let vertex_attributes _ = []
+  let vertex_name v = "\"" ^ string_of_vertex v ^ "\""
+  let default_vertex_attributes _ = []
+  let graph_attributes _ = []
+end)
+
+let rec find_label_index instructions label =
+  match instructions with
+  | [] -> raise (Failure "Not Found")
+  | Label a :: t ->
+      if String.compare label a = 0 then 0 else 1 + find_label_index t label
+  | _ :: t -> 1 + find_label_index t label
+
+let rec init_table instructions nodes i =
+  match instructions with
+  | [] -> nodes
+  | h :: t ->
+      let v = G.V.create (h, i) in
+      let nodes = Map.add_exn ~key:i ~data:v nodes in
+      init_table t nodes (i + 1)
+
+let rec init_nodes instructions g i =
+  match instructions with
+  | [] -> g
+  | hd :: tl ->
+      let v = G.V.create (hd, i) in
+      let g = G.add_vertex g v in
+      init_nodes tl g (i + 1)
+
+let rec add_edges g done_list work_list nodes i =
+  match work_list with
+  | [] | [ _ ] -> g
+  | h :: tl -> (
+      let v = Map.find_exn nodes i in
+      match h with
+      | Goto a ->
+          let target_node =
+            Map.find_exn nodes (find_label_index (done_list @ work_list) a)
+          in
+          let g = G.add_edge g v target_node in
+          add_edges g (done_list @ [ h ]) tl nodes (i + 1)
+      | Breq (a, _, _)
+      | Brneq (a, _, _)
+      | Brgt (a, _, _)
+      | Brlt (a, _, _)
+      | Brgeq (a, _, _)
+      | Brleq (a, _, _) ->
+          let t =
+            Map.find_exn nodes (find_label_index (done_list @ work_list) a)
+          in
+          let f = Map.find_exn nodes (i + 1) in
+          let g = G.add_edge g v t in
+          let g = G.add_edge g v f in
+          add_edges g (done_list @ [ h ]) tl nodes (i + 1)
+      | _ ->
+          let target_node = Map.find_exn nodes (i + 1) in
+          let g = G.add_edge g v target_node in
+          add_edges g (done_list @ [ h ]) tl nodes (i + 1))
+
+module Map = Map.Make (Int)
+
+let make_cfg func =
+  let instructions = func.code_body in
+  let g = init_nodes instructions G.empty 0 in
+  let nodes = init_table instructions Map.empty 0 in
+  add_edges g [] instructions nodes 0
--- bin/main.ml
+++ bin/main.ml
@@ -1,32 +1,40 @@
-(** Main entry point for our application. *)
 open Core
+(** Main entry point for our application. *)
+
 open Lexer
 open Lexing
 open Syntax
+open Cfg
+
+let print_cfg cfg =
+  let file = Caml.open_out "cfg.dot" in
+  Dot.output_graph file cfg
 
 let print_position outx lexbuf =
   let pos = lexbuf.lex_curr_p in
-  fprintf outx "%s:%d:%d" pos.pos_fname
-    pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+  fprintf outx "%s:%d:%d" pos.pos_fname pos.pos_lnum
+    (pos.pos_cnum - pos.pos_bol + 1)
 
 let parse_with_error lexbuf =
   try Parser.prog Lexer.read lexbuf with
   | SyntaxError msg ->
-    fprintf stderr "%a: %s\n" print_position lexbuf msg;
-    []
+      fprintf stderr "%a: %s\n" print_position lexbuf msg;
+      []
   | Parser.Error ->
-    fprintf stderr "%a: syntax error\n" print_position lexbuf;
-    exit (-1)
-    
+      fprintf stderr "%a: syntax error\n" print_position lexbuf;
+      exit (-1)
+
 let rec parse_and_print lexbuf =
   match parse_with_error lexbuf with
   | [] -> ()
-  | [a] ->
-    printf "test1%s\n" a.name;
-    parse_and_print lexbuf
+  | [ a ] ->
+      printf "test1%s\n" a.name;
+      parse_and_print lexbuf;
+      let cfg = Cfg.make_cfg a in
+      print_cfg cfg
   | a :: _ ->
-    printf "test2%s\n" a.name;
-    parse_and_print lexbuf
+      printf "test2%s\n" a.name;
+      parse_and_print lexbuf
 
 let loop filename () =
   let inx = In_channel.create filename in
@@ -40,5 +48,3 @@ let () =
     Command.Spec.(empty +> anon ("filename" %: string))
     loop
   |> Command.run
-
-
